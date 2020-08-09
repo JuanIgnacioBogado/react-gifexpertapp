7fcdb8a92cb08b8ba9303664054a7bf3d6c70005
Juan Ignacio <juan.bogado.8@hotmail.com>
2020-08-09 12:16:14 -0300

    add docs

---
+++ docs/precache-manifest.4bcf485aea47771f2b7fa91546787af1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa8752b8788c8f7378ec827664e4b3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6695d050717860daa4eb",
+    "url": "/static/css/main.b3d6839f.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6695d050717860daa4eb",
+    "url": "/static/js/main.572e943f.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
